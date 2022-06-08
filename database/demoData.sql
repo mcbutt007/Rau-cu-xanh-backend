da3b8b03134227b8e69c55ead1df75ef08b6290d
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-06-08 17:50:20 +0700

    added requirements

--- database/demoData.sql
+++ database/demoData.sql
@@ -1,1 +1,2 @@
 INSERT INTO RAUCU values (null,"Rau muống","rau",1,5000,"Rau muống là loại giống cây thân thảo, mọc bò trên mặt nước hoặc trên đất bùn.",null,CURRENT_TIMESTAMP,null,20)
+INSERT INTO SHOP values (null,"Rau Cu Xanh Official","abc xyz",123456,CURRENT_TIMESTAMP,4,null,500)
