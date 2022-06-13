e8fbdebf5167e3efca2ebea1c9c8f8ee0aeec5b4
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-06-13 07:02:07 +0700

    add data

--- app.py
+++ app.py
@@ -40,12 +40,111 @@ def Home(one=False):
     conn.close()
     return jsonify((r[0] if response else None) if one else response)
 
+# trả về danh sách Bookmark
+@app.route('/bookmark', methods=['POST','GET'])
+def Bookmark(one=False):
+    conn = get_db_connection()
+    feed = conn.execute('SELECT * FROM BOOKMARK')
+    # convert query to json
+    response = [dict((feed.description[i][0], value) \
+        for i, value in enumerate(row)) for row in feed.fetchall()]
+    conn.close()
+    return jsonify((r[0] if response else None) if one else response)
+
+# trả về danh sách Cart
+@app.route('/cart', methods=['POST','GET'])
+def Cart(one=False):
+    conn = get_db_connection()
+    feed = conn.execute('SELECT * FROM CART')
+    # convert query to json
+    response = [dict((feed.description[i][0], value) \
+        for i, value in enumerate(row)) for row in feed.fetchall()]
+    conn.close()
+    return jsonify((r[0] if response else None) if one else response)
+
+# trả về danh sách FB_email
+@app.route('/forgotpassword_email', methods=['POST','GET'])
+def Forgotpassword_email(one=False):
+    conn = get_db_connection()
+    feed = conn.execute('SELECT * FROM FORGET_PASSWORD_EMAIL')
+    # convert query to json
+    response = [dict((feed.description[i][0], value) \
+        for i, value in enumerate(row)) for row in feed.fetchall()]
+    conn.close()
+    return jsonify((r[0] if response else None) if one else response)
+
+# trả về danh sách NOTIFICATIONS
+@app.route('/notifications', methods=['POST','GET'])
+def Notifications(one=False):
+    conn = get_db_connection()
+    feed = conn.execute('SELECT * FROM NOTIFICATIONS')
+    # convert query to json
+    response = [dict((feed.description[i][0], value) \
+        for i, value in enumerate(row)) for row in feed.fetchall()]
+    conn.close()
+    return jsonify((r[0] if response else None) if one else response)
+
+# trả về danh sách reeciept
+@app.route('/reciept', methods=['POST','GET'])
+def Reciept(one=False):
+    conn = get_db_connection()
+    feed = conn.execute('SELECT * FROM RECIEPT')
+    # convert query to json
+    response = [dict((feed.description[i][0], value) \
+        for i, value in enumerate(row)) for row in feed.fetchall()]
+    conn.close()
+    return jsonify((r[0] if response else None) if one else response)
+
+# trả về danh sách reciept raucu list
+@app.route('/reciept_raucu_list', methods=['POST','GET'])
+def Reciept_raucu_list(one=False):
+    conn = get_db_connection()
+    feed = conn.execute('SELECT * FROM RECIEPT_RAUCU_LIST')
+    # convert query to json
+    response = [dict((feed.description[i][0], value) \
+        for i, value in enumerate(row)) for row in feed.fetchall()]
+    conn.close()
+    return jsonify((r[0] if response else None) if one else response)
+
+# trả về danh sách review
+@app.route('/review', methods=['POST','GET'])
+def Review(one=False):
+    conn = get_db_connection()
+    feed = conn.execute('SELECT * FROM REVIEW')
+    # convert query to json
+    response = [dict((feed.description[i][0], value) \
+        for i, value in enumerate(row)) for row in feed.fetchall()]
+    conn.close()
+    return jsonify((r[0] if response else None) if one else response)
+    
+    # trả về danh sách Shop
+@app.route('/shop', methods=['POST','GET'])
+def Shop(one=False):
+    conn = get_db_connection()
+    feed = conn.execute('SELECT * FROM SHOP')
+    # convert query to json
+    response = [dict((feed.description[i][0], value) \
+        for i, value in enumerate(row)) for row in feed.fetchall()]
+    conn.close()
+    return jsonify((r[0] if response else None) if one else response)
+
+    # trả về danh sách Shop raucu list
+@app.route('/shop_raucu_list', methods=['POST','GET'])
+def Shop_raucu_list(one=False):
+    conn = get_db_connection()
+    feed = conn.execute('SELECT * FROM SHOP_RAUCU_LIST')
+    # convert query to json
+    response = [dict((feed.description[i][0], value) \
+        for i, value in enumerate(row)) for row in feed.fetchall()]
+    conn.close()
+    return jsonify((r[0] if response else None) if one else response)
+
 # trả về thông tin rau củ theo id
 @app.route('/raucuinfo/<id>', methods=['GET','POST'])
 def RaucuInfo(id,one=False):
     conn = get_db_connection()
     raucuinfo = conn.execute('SELECT * FROM RAUCU WHERE RAUCU_ID = ?',
-                            (id))
+                            (id,))
     response = [dict((raucuinfo.description[i][0], value) \
         for i, value in enumerate(row)) for row in raucuinfo.fetchall()]
     conn.close()
@@ -65,7 +164,7 @@ def UserInfo(id,one=False):
     return jsonify((r[0] if response else None) if one else response)
 
 # login trả về userid nếu thành công
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST','GET'])
 def check_login():
     #get username from POST request ex: /checklogin?username=Phuc&password=123456
     username = request.args.get('username')
@@ -119,6 +218,17 @@ def recieve_registration_info():
         return "Đã tạo tài khoản thành công"
     else :
         return "Email tồn tại. Có phải bạn quên mật khẩu?"
+
+@app.route('/muahang', methods=['POST','GET'])
+def MuaHang():
+    userid = request.args.get('userid')
+    raucuid = request.args.get('raucuid')
+
+    conn = get_db_connection()
+    conn.execute('INSERT INTO RECIEPT (USER_ID,RAUCU_ID) VALUES (?,?)', (userid,raucuid))
+    conn.commit()
+    conn.close()
+    return "Done"
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
