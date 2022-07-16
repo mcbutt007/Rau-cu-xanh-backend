4e21c0116a96854f3c02a0d6d59346d2142a04f5
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-07-16 09:28:46 +0700

    saving progress

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from app import db
 
 class User(db.Model):
-    UserID = db.Column(db.Integer, primary_key=True)
+    UserID = db.column(db.integer, primary_key=true)
     UserName = db.Column(db.String(64), index=True, unique=True)
     Email = db.Column(db.String(120), index=True, unique=True)
     PasswordHash = db.Column(db.String(128))
@@ -19,7 +19,7 @@ class Reset_Password_Email(db.Model):
 class Notifications(db.Model):
     NotiID = db.Column(db.Integer, primary_key=True)
     UserID = db.Column(db.Integer, db.ForeignKey('user.UserID'))
-    RaucuID = db.Column(db.Integer, db.ForeignKey('shop.ShopID'))
+    RaucuID = db.Column(db.Integer, db.ForeignKey('raucu.RaucuID'))
     TypeID = db.Column(db.Integer, db.ForeignKey('noti_type.NotiTypeID'))
     Description = db.Column(db.String(512))
     TimeStamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -31,59 +31,58 @@ class Noti_Type(db.Model):
 
 class Reciept(db.Model):
     RecieptID = db.Column(db.Integer, primary_key=True)
-    UserID = db.Column(db.String(64), index=True, unique=True)
-    ShippingCost = db.Column(db.String(120), index=True, unique=True)
-    ShippingAddr = db.Column(db.String(128))
-    TotalPrice = db.Column(db.String(128))
-    CreatedTime = db.Column(db.Numeric)
+    UserID = db.Column(db.Integer, db.ForeignKey('user.UserID'))
+    ShippingCost = db.Column(db.Numeric)
+    ShippingAddr = db.Column(db.Numeric)
+    TotalPrice = db.Column(db.Numeric)
+    CreatedTime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 class Reciept_List(db.Model):
     RecieptID = db.Column(db.Integer, primary_key=True)
-    RaucuID = db.Column(db.String(64), index=True, unique=True)
-    Quantity = db.Column(db.String(120), index=True, unique=True)
+    RaucuID = db.Column(db.Integer, db.ForeignKey('raucu.RaucuID'))
+    Quantity = db.Column(db.Integer)
 
 class Cart(db.Model):
     CartID = db.Column(db.Integer, primary_key=True)
-    UserID = db.Column(db.String(64), index=True, unique=True)
-    RaucuID = db.Column(db.String(120), index=True, unique=True)
-    Quantity = db.Column(db.String(120), index=True, unique=True)
-    TimeStamp = db.Column(db.String(120), index=True, unique=True)
+    UserID = db.Column(db.Integer, db.ForeignKey('user.UserID'))
+    RaucuID = db.Column(db.Integer, db.ForeignKey('raucu.RaucuID'))
+    Quantity = db.Column(db.Integer)
+    TimeStamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 class Bookmark(db.Model):
     BookmarkID = db.Column(db.Integer, primary_key=True)
-    UserID = db.Column(db.String(64), index=True, unique=True)
-    RaucuID = db.Column(db.String(120), index=True, unique=True)
-    TimeStamp = db.Column(db.String(120), index=True, unique=True)
+    UserID = db.Column(db.Integer, db.ForeignKey('user.UserID'))
+    RaucuID = db.Column(db.Integer, db.ForeignKey('raucu.RaucuID'))
+    TimeStamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 class Review(db.Model):
     ReviewID = db.Column(db.Integer, primary_key=True)
-    UserID = db.Column(db.String(64), index=True, unique=True)
-    Stars = db.Column(db.String(120), index=True, unique=True)
-    Comments = db.Column(db.String(120), index=True, unique=True)
-    ReviewType = db.Column(db.String(120), index=True, unique=True)
-    TimeStamp = db.Column(db.String(120), index=True, unique=True)
+    UserID = db.Column(db.Integer, db.ForeignKey('user.UserID'))
+    Stars = db.Column(db.Float)
+    Comments = db.Column(db.String(512))
+    ReviewType = db.Column(db.Integer, db.ForeignKey('reviewtype.TypeID'))
+    TimeStamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 class ReviewType(db.Model):
     TypeID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(64), index=True, unique=True)
-    RaucuID = db.Column(db.String(120), index=True, unique=True)
-    ShopID = db.Column(db.String(120), index=True, unique=True)
-    TimeStamp = db.Column(db.String(120), index=True, unique=True)
+    RaucuID = db.Column(db.Integer, db.ForeignKey('raucu.RaucuID'))
+    ShopID = db.Column(db.Integer, db.ForeignKey('shop.ShopID'))
 
 class Shop(db.Model):
     ShopID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(64), index=True, unique=True)
-    Address = db.Column(db.String(120), index=True, unique=True)
-    PhoneNo = db.Column(db.String(120), index=True, unique=True)
-    ProfilePic = db.Column(db.String(120), index=True, unique=True)
-    TimeCreated = db.Column(db.String(120), index=True, unique=True)
-    NoSelling = db.Column(db.String(120), index=True, unique=True)
-    NoSold = db.Column(db.String(120), index=True, unique=True)
-    ReviewID = db.Column(db.String(120), index=True, unique=True)
+    Address = db.Column(db.String(256), index=True)
+    PhoneNo = db.Column(db.Numeric)
+    ProfilePic = db.Column(db.String(120))
+    TimeCreated  = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    NoSelling = db.Column(db.Integer)
+    NoSold = db.Column(db.Integer)
+    ReviewID = db.Column(db.Integer, db.ForeignKey('review.ReviewID'))
 
 class Selling_List(db.Model):
-    ShopID = db.Column(db.Integer, primary_key=True)
-    RaucuID = db.Column(db.String(64), index=True, unique=True)
+    ShopID = db.Column(db.Integer, db.ForeignKey('shop.ShopID'))
+    RaucuID = db.Column(db.Integer, db.ForeignKey('raucu.RaucuID'))
     Quantity = db.Column(db.String(120), index=True, unique=True)
     Status = db.Column(db.String(120), index=True, unique=True)
     ItemSold = db.Column(db.String(120), index=True, unique=True)
@@ -92,10 +91,10 @@ class Raucu(db.Model):
     RaucuID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(64), index=True, unique=True)
     Type = db.Column(db.String(120), index=True, unique=True)
-    ShopID = db.Column(db.String(120), index=True, unique=True)
+    ShopID = db.Column(db.Integer, db.ForeignKey('shop.ShopID'))
     ProductPic = db.Column(db.String(120), index=True, unique=True)
     Price = db.Column(db.String(120), index=True, unique=True)
     Description = db.Column(db.String(120), index=True, unique=True)
     ReviewID = db.Column(db.String(120), index=True, unique=True)
-    TimeCreated = db.Column(db.String(120), index=True, unique=True)
+    TimeCreated = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     Discount = db.Column(db.String(120), index=True, unique=True)
